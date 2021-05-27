782b5c96ae4a06245587825937492b7fb6b9021a
Chris <mistercwood@gmail.com>
2021-05-27 11:55:53 +1000

    Added function to paint squares with random colour

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function gridGenerate(squares) {
 }
 
 function paintSquare(e) {
-    e.target.style.backgroundColor = "#666";
+    e.target.style.backgroundColor = rainbow();
 }
 
 let currentSquare = document.getElementById("gridContainer");
@@ -23,6 +23,17 @@ function shadeSquare() {
     let shades = ["hsl(0, 0%, 100%", "hsl(0, 0%, 90%", "hsl(0, 0%, 80%", "hsl(0, 0%, 70%", 
     "hsl(0, 0%, 60%", "hsl(0, 0%, 50%", "hsl(0, 0%, 40%", "hsl(0, 0%, 30%", 
     "hsl(0, 0%, 20%", "hsl(0, 0%, 10%", "hsl(0, 0%, 0%"];
+}
+
+function rainbow() {
+    let colourElements = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+ 'B', 'C', 'D', 'E', 'F'];
+    let currentColour = ['#'];
+        for (i = 0; i < 6; i++) {
+        num = colourElements[Math.floor(Math.random() * 16)];
+        currentColour.push(num);
+    }
+    return(currentColour.join(""));
 }
 
 gridGenerate(16);
